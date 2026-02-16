dadda327dad495659b34bdee8ee00e68a4554a69
adyuta447 <yutaagra@gmail.com>
2026-02-16 18:51:37 +0700

    solved all cases

--- optional-07-real-world-scenario/orders.js
+++ optional-07-real-world-scenario/orders.js
@@ -3,20 +3,50 @@ function generateUniqueId() {
   return `_${Math.random().toString(36).slice(2, 9)}`;
 }
 
+// Variabel untuk menyimpan data orders
+let orders = [];
 
-// TODO: buatlah variabel yang menampung data orders
-let orders;
+// Fungsi menambahkan order baru
+function addOrder(customerName, items) {
+  const totalPrice = items.reduce((total, item) => total + item.price, 0);
 
-// TODO: selesaikan fungsi addOrder
-function addOrder(customerName, items) {}
+  const newOrder = {
+    id: generateUniqueId(),
+    customerName,
+    items,
+    totalPrice,
+    status: "Menunggu",
+  };
 
-// TODO: selesaikan fungsi updateOrderStatus
-function updateOrderStatus(orderId, status) {}
+  orders.push(newOrder);
+  return newOrder;
+}
 
-// TODO: selesaikan fungsi calculateTotalRevenue dari order yang berstatus Selesai
-function calculateTotalRevenue() {}
+// Fungsi update status order
+function updateOrderStatus(orderId, status) {
+  const order = orders.find((order) => order.id === orderId);
 
-// TODO: selesaikan fungsi deleteOrder
-function deleteOrder(id) {}
+  if (!order) return;
 
-export { orders, addOrder, updateOrderStatus, calculateTotalRevenue, deleteOrder };
+  order.status = status;
+}
+
+// Fungsi menghitung total pendapatan dari order yang statusnya "Selesai"
+function calculateTotalRevenue() {
+  return orders
+    .filter((order) => order.status === "Selesai")
+    .reduce((total, order) => total + order.totalPrice, 0);
+}
+
+// Fungsi menghapus order berdasarkan id
+function deleteOrder(id) {
+  orders = orders.filter((order) => order.id !== id);
+}
+
+export {
+  orders,
+  addOrder,
+  updateOrderStatus,
+  calculateTotalRevenue,
+  deleteOrder,
+};
